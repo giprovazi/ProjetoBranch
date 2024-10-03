269a9e7de4773ab9a668655eab91fd38eeb1f46c
giovanni <giovannirprovazi@gmail.com>
2024-10-03 11:24:37 -0300

    Criando funcao em Javascript

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 // ESTRUTURA DE REPETIÇÃO
 
 for(let i=0; i<10;i++){
-    console.log(i)
+    // console.log(i)
 }
-            
+
 // WHILE
 
 let f = 0;
 while(f < 10){
-    console.log(f)
+    // console.log(f)
     f++
 }
+
+// DO WHILE
+
+let w=0;
+
+do{
+    console.log(w);
+    w++;
+}
+while(w<5);
+
+// FUNÇÕES
+
+function Ola(nome){
+    // `` (strings literal ou template string ${}) chama a variável
+    console.log(`seja bem vindo ${nome}`)
+    alert(`seja bem vindo a ${nome}`)
+    document.write(`seja bem vindo a ${nome}`)
+}
+// executando a função Ola passando o valor
+Ola("fiap")
